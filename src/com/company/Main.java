01dca491c29ac3d4e5cbfd6871afb46eaff0a95a
YSilahin <egor.silagin96@gmail.com>
2021-06-11 13:06:07 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s = "";
+
+
+        s = String.format("%-24s| %-9s| min = %,-27d| max = %,-27d|",
+                Byte.class, "1 byte", Byte.MIN_VALUE, Byte.MAX_VALUE);
+        System.out.println(s);
+
+
+        s = String.format("%-24s| %-9s| min = %,-27d| max = %,-27d|",
+                Short.class, "2 bytes", Short.MIN_VALUE, Short.MAX_VALUE);
+        System.out.println(s);
+
+
+        s = String.format("%-24s| %-9s| min = %,-27d| max = %,-27d|",
+                Integer.class, "4 bytes", Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.println(s);
+
+
+        s = String.format("%-24s| %-9s| min = %,-27d| max = %,-27d|",
+                Long.class, "8 bytes", Long.MIN_VALUE, Long.MAX_VALUE);
+        System.out.println(s);
+
+    }
+}
